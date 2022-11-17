a200f376446b86991b2d9556978ff30ff1cead1a
João Paulo <johnowl@gmail.com>
2022-11-17 23:13:34 +0100

    add counter with more complex model

---
+++ counter2/src/Main.elm
@@ -0,0 +1,41 @@
+module Main exposing (..)
+
+import Browser exposing(sandbox)
+import Html exposing(..)
+import Html.Events exposing(onClick)
+
+
+main =
+    sandbox {
+        init = init
+        , update = update
+        , view = view
+    }
+
+type alias Model = 
+    { total: Int }
+
+
+init : Model
+init = 
+    { total = 0 }
+
+type Msg = Decrement | Increment
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Decrement  ->
+            { model | total = model.total - 1 }
+
+        Increment ->
+            { model | total = model.total + 1 }
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ button [ onClick Decrement ] [ text "-" ]
+    , div [] [ text (String.fromInt model.total) ]
+    , button [ onClick Increment ] [ text "+" ]
+    ]
